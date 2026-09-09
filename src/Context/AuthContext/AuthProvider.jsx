dc01c43187eca10c6c049112a8e0ae8dab0a720e
rafsan15-10903 <rafsan15-10903@diu.edu.bd>
2026-09-09 16:10:26 +0600

    useLocation implement

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../Firebase/firebase.init';
+import { GoogleAuthProvider } from 'firebase/auth';
 
+const googleProvider = new GoogleAuthProvider()
 const AuthProvider = ({children}) => {
     const[user,setUser]=useState(null)
     const [loading,setLoading]=useState(true)
 
-    const createUser = (email,password)=>{return createUserWithEmailAndPassword(auth,email,password)
+    const createUser = (email,password)=>{
         setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const googleSignIn = ()=>{
+        setLoading(true)
+        return signInWithPopup(auth,googleProvider)
     }
 
     const signInUser = (email,password)=>{
-        return signInWithEmailAndPassword(auth,email,password)
         setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
     }
 
     const signOutUser = ()=>{
-        return signOut(auth)
         setLoading(true)
+        return signOut(auth)
     }
 
     useEffect(()=>{
@@ -36,6 +44,7 @@ setLoading(false)
     user,
     signOutUser,
     loading,
+    googleSignIn,
     }
 //     onAuthStateChanged(auth, (currentUser)=>{
 // if(currentUser){
